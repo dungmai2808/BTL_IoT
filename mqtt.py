cb8731005f45375f2312bd85d4836261667b6762
dung2808 <dung.mai2808@hcmut.edu.vn>
2024-06-07 14:48:50 +0700

    Initial Commit

---
+++ mqtt.py
@@ -0,0 +1,142 @@
+import sys
+from Adafruit_IO import MQTTClient
+import time
+import json
+from data import *
+
+AIO_FEED_ID = ["scheduler", "deletesch", "broken", "sensor"]
+AIO_USERNAME = "Vy2003"
+AIO_KEY = ""
+
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_ID:
+        client.subscribe(topic)
+
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    global isRelayActive, isSensorActive
+
+    print("Nhan du lieu: " + payload)
+    if feed_id == "scheduler": #add scheduler
+        temp_data = json.loads(payload)
+        nodeSch = RNodeScheduler(temp_data)
+        waitingList.add(nodeSch)
+        waitingList.print_list()
+    if feed_id == "deletesch": #delete scheduler
+        waitingList.delete(payload)
+        waitingList.print_list()
+    if feed_id == "broken":
+        if payload != "0":
+            isRelayActive = False
+        else:
+            isRelayActive = True
+    if feed_id == "sensor":
+        if payload != "0":
+            isSensorActive = False
+        else:
+            isSensorActive = True
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
+client.publish("broken", 0)  #hư thì gửi khác 0
+client.publish("sensor", 0)  #hư thì gửi 1
+
+
+try:
+    ser = serial.Serial(port=getPort(), baudrate=9600)
+    print(ser)
+    print("Open successfully")
+except:
+    print("Can not open the port")
+
+isRelayActive = True
+isSensorActive = True
+waitingList = LinkedList()
+sensorNode = SNodeScheduler("Scheduler 0", datetime.now() + timedelta(seconds=10))
+waitingList.add(sensorNode)
+next_sensor_execute = datetime.now() + timedelta(seconds=10)
+
+while True:
+    if not isSensorActive and not isRelayActive:
+        # if both sensor and relays have error, wait 1s before checking again
+        time.sleep(1)
+
+    elif isSensorActive and isRelayActive:
+        if datetime.now() < waitingList.head.startTime:
+            # if not up to now, wait 1s before checking again
+            time.sleep(1)
+        else:
+            currentNode = waitingList.head
+            if currentNode.type == "relay":
+                isRelayActive = currentNode.task.Task_Execute(ser)
+            elif currentNode.type == "sensor":
+                isSensorActive = currentNode.task.Task_Execute(ser)
+
+            if currentNode.type == "relay" and isRelayActive:
+                print("{}: {} is done".format(datetime.now(), currentNode.name))
+                currentNode.cycle = currentNode.cycle - 1
+                # if relay task do all its cycle
+                if currentNode.cycle <= 0:
+                    currentNode.startTime = currentNode.task.startTime + timedelta(days=1)
+                    currentNode.cycle = currentNode.task.cycle
+                else:
+                    currentNode.startTime = datetime.now()
+                # if current node was not deleted, delete and add again
+                if waitingList.isAvailable(currentNode.name):
+                    waitingList.delete(currentNode.name)
+                    currentNode.next = None
+                    waitingList.add(currentNode)
+            elif currentNode.type == "sensor" and isSensorActive:
+                next_sensor_execute = currentNode.startTime = currentNode.startTime + timedelta(minutes=2)
+                waitingList.delete(currentNode.name)
+                currentNode.next = None
+                waitingList.add(currentNode)
+            waitingList.print_list()    
+
+    elif isRelayActive:
+        currentNode = waitingList.findFirst("relay")
+        if datetime.now() < currentNode.startTime:
+            time.sleep(1)
+        else:
+            isRelayActive = currentNode.task.Task_Execute(ser)
+            if isRelayActive:
+                print("{}: {} is done".format(datetime.now(), currentNode.name))
+                currentNode.cycle = currentNode.cycle - 1
+                # if relay task do all its cycle
+                if currentNode.cycle <= 0:
+                    currentNode.startTime = currentNode.task.startTime + timedelta(days=1)
+                    currentNode.cycle = currentNode.task.cycle
+                else:
+                    currentNode.startTime = datetime.now()
+                # if current node was not deleted, delete and add again
+                if waitingList.isAvailable(currentNode.name):
+                    waitingList.delete(currentNode.name)
+                    waitingList.add(currentNode)  
+                    currentNode.next = None
+                    waitingList.print_list()
+
+    else: # isSensorActive == True
+        if datetime.now() < next_sensor_execute:
+            time.sleep(1)
+        else:
+            currentNode = waitingList.findFirst("sensor")
+            isSensorActive = currentNode.task.Task_Execute(ser)
+            if isSensorActive:
+                next_sensor_execute = currentNode.startTime = currentNode.startTime + timedelta(minutes=2)
+                waitingList.delete(currentNode.name)
+                currentNode.next = None
+                waitingList.add(currentNode)
+                waitingList.print_list()
---
+++ things.py
@@ -0,0 +1,105 @@
+from math import ceil
+from crc import *
+from my_serial import *
+
+
+class Relay:
+    def __init__(self, id):
+        self.id = id
+        self.relay_ON = [0, 6, 0, 0, 0, 255, 0, 0]
+        self.relay_OFF = [0, 6, 0, 0, 0, 0, 0, 0]
+        self.relay_ON[0] = self.relay_OFF[0] = id
+        add_crc16_modbus(self.relay_ON)
+        add_crc16_modbus(self.relay_OFF)
+
+    def data_format(self):
+        print("Data for relay {} on is: {}".format(self.id, self.relay_ON))
+        print("Data for relay {} off is: {}".format(self.id, self.relay_OFF))
+
+    def turnRelayOn(self, ser):
+        ser.write(self.relay_ON)
+        print("On: {}".format(self.relay_ON))
+
+    def turnRelayOff(self, ser):
+        ser.write(self.relay_OFF)
+        print("Off: {}".format(self.relay_OFF))
+
+
+class MixerRelay(Relay):
+    flow = 5  # 5 ml/s
+    def __init__(self, id, fertilizer):
+        self.fertilizer = fertilizer
+        self.timer = ceil(self.fertilizer / self.flow)
+        super().__init__(id)
+
+    def turnRelayOn(self, ser):
+        if self.timer > 0:
+            super().turnRelayOn(ser)
+        return
+    
+    def turnRelayOff(self, ser):
+        if self.timer > 0:
+            super().turnRelayOff(ser)
+        return
+
+
+class AreaRelay(Relay):
+    def __init__(self, areaSelector):
+        super().__init__(areaSelector + 3)  #id = area + 3 (ex: area 1 have id 4)
+
+
+class PumpRelay(Relay):
+    def __init__(self, id):
+        super().__init__(id)
+
+
+class Sensor:
+    def __init__(self, id):
+        self.id = id
+        self.soil_temp = [0, 3, 0, 6, 0, 1, 0, 0]
+        self.soil_humid = [0, 3, 0, 7, 0, 1, 0, 0]
+        self.soil_temp[0] = self.soil_humid[0] = id
+        add_crc16_modbus(self.soil_temp)
+        add_crc16_modbus(self.soil_humid)
+
+    def data_format(self):
+        print("Data for temp senor {} is: {}".format(self.id, self.soil_temp))
+        print("Data for humid sensor {} is: {}".format(self.id, self.soil_humid))
+
+    def getTemp(self, ser):
+        ser.write(self.soil_temp)
+        print(self.soil_temp)
+
+    def getHumid(self, ser):
+        ser.write(self.soil_humid)
+        print(self.soil_humid)
+
+# try:
+#     ser = serial.Serial(port=getPort(), baudrate=9600)
+#     print(ser)
+#     print("Open successfully")
+# except:
+#     print("Can not open the port")
+
+
+# def main(args=None):
+#     mixerRelay1 = MixerRelay(1, 20)
+#     mixerRelay2 = MixerRelay(2, 10)
+#     mixerRelay3 = MixerRelay(3, 20)
+
+#     print("Timer for mixer {} is {}".format(mixerRelay1.id, mixerRelay1.timer))
+#     print("Timer for mixer {} is {}".format(mixerRelay2.id, mixerRelay2.timer))
+#     print("Timer for mixer {} is {}".format(mixerRelay3.id, mixerRelay3.timer))
+
+#     mixerRelay1.data_format()
+#     mixerRelay2.data_format()
+#     mixerRelay3.data_format()
+
+#     sensor = Sensor(10)
+#     sensor.data_format()
+
+#     sensor.getTemp(ser)
+
+
+# if __name__ == '__main__':
+#     main()
